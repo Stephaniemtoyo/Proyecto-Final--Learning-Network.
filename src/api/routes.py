3d7a6b21db5ff5f08b153a1c57277cbfdaa0b0ab
Virecny Stephanie Mata Toyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-09-18 13:02:45 +0200

    Initialize barraBusqueda

---
+++ src/api/routes.py
@@ -0,0 +1,140 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, jwt_required
+from api.models import db, User, Curso
+
+# Crear el Blueprint para la API
+api = Blueprint('api', __name__)
+
+@api.route('/')
+def root():
+    return "Home"
+
+@api.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({"msg": user.serialize()}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@api.route('/users/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    if users:
+        return jsonify({"msg": [user.serialize() for user in users]}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if not data or 'id' not in data or 'name' not in data or 'telefono' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+    user_id = data['id']
+    if User.query.get(user_id):
+        return jsonify({"error": "User already exists"}), 400
+    new_user = User(id=user_id, name=data['name'], telefono=data['telefono'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"status": "User created", "user": new_user.serialize()}), 201
+
+@api.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    if not data or 'name' not in data or 'telefono' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    user.name = data['name']
+    user.telefono = data['telefono']
+    db.session.commit()
+    return jsonify({"status": "User updated", "user": user.serialize()}), 200
+
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"status": "User deleted"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+# Rutas protegidas por JWT
+@api.route('/cursos', methods=['POST'])
+#@jwt_required() AUN NO SE SI HACE FALTA
+def crear_curso():
+    title = request.json.get('title', None)
+    portada = request.json.get('portada', None)
+    resumen = request.json.get('resumen', None)
+    categoria = request.json.get('categoria', None)
+    nivel = request.json.get('nivel', None)
+    idioma = request.json.get('idioma', None)
+    modulos = request.json.get('modulos', None)
+    if not title or not portada or not resumen or not categoria or not nivel or not idioma:
+        return jsonify({'success': False, 'msg': 'Todos los campos son necesarios'}), 400
+    curso = Curso.query.filter_by(title=title).first()
+    if curso:
+        return jsonify({'success': False, 'msg': 'El curso ya existe, intenta otro título'}), 400
+    new_curso = Curso(title=title, portada=portada, resumen=resumen, categoria=categoria, nivel=nivel,idioma=idioma)
+    db.session.add(new_curso)
+    db.session.commit()
+    return jsonify({'success': True, 'curso': new_curso.serialize()}), 200
+
+@api.route('/cursos/<int:curso_id>', methods=['GET'])
+#@jwt_required() AUN NO SE SI HACE FALTA
+def get_curso(curso_id):
+    curso = Curso.query.get(curso_id)
+    if curso:
+        return jsonify({"msg": curso.serialize()}), 200
+    else:
+        return jsonify({"error": "Curso not found"}), 404
+
+@api.route('/cursos', methods=['GET'])
+#@jwt_required() AUN NO SE SI HACE FALTA
+def get_cursos():
+    cursos = Curso.query.all()
+    if cursos:
+        return jsonify({"msg": [curso.serialize() for curso in cursos]}), 200
+    else:
+        return jsonify({"error": "Cursos not found"}), 404
+
+@api.route('/login', methods=['POST'])
+def login():
+    print("Request JSON:", request.json)
+    email = request.json.get('email')
+    password = request.json.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        if user.password == password:
+            access_token = create_access_token(identity=user.id)
+            return jsonify({'success': True, 'user': user.serialize(), 'token': access_token}), 200
+        return jsonify({'success': False, 'msg': 'Combinación usuario/contraseña no es válida'}), 400
+    return jsonify({'success': False, 'msg': 'El correo electrónico no tiene una cuenta asociada'}), 404
+
+
+@api.route('/signup', methods=['POST'])
+def signup():
+
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    type = request.json.get('type', None)
+    if not email or not password or not type or type is None:
+        return jsonify({'success': False, 'msg': 'Todos los campos son necesarios'}), 400
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({'success': False, 'msg': 'El correo electrónico ya tiene una cuenta, intenta iniciar sesión'}), 400
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    access_token = create_access_token(identity=new_user.id)
+    if type == 'alumno':
+        #crear registro en tabla de alumno con este id como user
+        print(type)
+        return jsonify({'success': True, 'user': new_user.serialize(), 'token': access_token}), 200
+    if type== 'profesor':
+        #crear registro en tabla de profesor con este id como user
+        print(type)
+        return jsonify({'success': True, 'user': new_user.serialize(), 'token': access_token}), 200
---
+++ src/front/js/pages/home.js
@@ -0,0 +1,103 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+//Barra de búsqueda
+import BarraBusqueda from "../component/barraBusqueda";
+import "../../styles/barraBusqueda.css";
+import ListaCursos from "../component/listaCursos";
+import Testimonios from "../component/testimonios";
+import "../../styles/testimonios.css";
+
+
+
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+// esto para que al presionar la pestaña, nos lleve a una pagina LINEA 12
+return (
+    <div>
+        <div id="carouselExampleFade" className="carousel slide carousel-fade">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <a href="https://example.com/page1" target="_blank" rel="noopener noreferrer">
+                        <img src="https://i.blogs.es/78408e/programador/1366_2000.jpeg" className="d-block w-100" alt="First Slide" />
+                    </a>
+                    <div className="text-overlay">Aprende, Conecta y Crece</div>
+                </div>
+                <div className="carousel-item">
+                    <a href="https://example.com/page2" target="_blank" rel="noopener noreferrer">
+                        <img src="https://images.pexels.com/photos/4050315/pexels-photo-4050315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="Second Slide" />
+                    </a>
+                </div>
+                <div className="carousel-item">
+                    <a href="https://example.com/page3" target="_blank" rel="noopener noreferrer">
+                        <img src="https://esden.es/wp-content/uploads/2024/06/lenguajesdeprogramacion-qbit-01.webp" className="d-block w-100" alt="Third Slide" />
+                    </a>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+        {/* Aquí se agrega el componente BarraBusqueda */}
+        <BarraBusqueda />
+        <ListaCursos />
+
+        <div className="container-center-elegirnos">
+            <div className="card-elegirnos mb-3">
+                <div className="row g-0">
+                <div className="imagen-elegirnos col-md-6">
+                <img src="https://images.pexels.com/photos/8199654/pexels-photo-8199654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-6">
+            <div className="card-body-elegirnos">
+                <h5 className="card-title-elegirnos">Da el primer paso hacia tu nueva carrera profesional</h5>
+                <p className="card-text">Comienza tu camino hacia una carrera gratificante y de gran demanda con nuestra capacitación de nivel profesional. No importa si estás empezando desde cero o buscando cambiar de sector, nuestro programa está diseñado para brindarte las habilidades y conocimientos necesarios para destacar.Al completar nuestro programa, obtendrás una credencial reconocida por las empresas más influyentes del sector, lo que te abrirá puertas en el competitivo mercado laboral. Este reconocimiento es un testimonio de tu dedicación y dominio de las habilidades que las empresas buscan en los profesionales de hoy.</p>
+                <p className="card-text"></p>
+            </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-center-elegirnos2">
+            <div className="card-elegirnos2 mb-3">
+                <div className="row g-0">
+                <div className="col-md-6">
+                <div className="card-body-elegirnos2">
+                <h5 className="card-title-elegirnos2">Experiencia previa opcional</h5>
+                <p className="card-text">No necesitas experiencia previa para comenzar. Ya sea que estés dando tus primeros pasos en un campo nuevo o buscando ampliar tu conjunto de habilidades actuales, nuestro enfoque accesible y comprensible está diseñado para todos. Desarrollarás habilidades esenciales y prácticas que te prepararán para el trabajo, incluso si apenas estás comenzando en el campo.</p>
+                <p className="card-text"></p>
+                </div>
+                </div>
+            <div className="imagen-elegirnos2 col-md-6">
+                <img src="https://images.pexels.com/photos/8199654/pexels-photo-8199654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="img-fluid rounded-start" alt="..." />
+            </div>
+                </div>
+            </div>
+            </div>
+   
+        <Testimonios />
+
+     <div className="ContenedorEmpresas">
+        <div className="BarraDeEmpresas">
+            <h4>Más de 7500 empresas y millones de estudiantes de todo el mundo confían en nosotros</h4>
+            <div className="icon-container mt-4">
+            <span className="fa-brands fa-apple"></span>
+            <span className="fa-brands fa-windows"></span>
+            <span className="fa-brands fa-google"></span>
+            <span className="fa-brands fa-playstation"></span>
+            <span className="fa-brands fa-youtube"></span>
+            <span className="fa-brands fa-instagram"></span>
+            <span className="fa-brands fa-paypal"></span>
+            <span className="fa-brands fa-linkedin"></span>
+            </div>
+        </div>
+        </div>
+</div>
+);
+};
